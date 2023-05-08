dd3ba66285e9a15b02cde2563f2364b1d40d7bd2
andyAG24 <andy.ag.24@gmail.com>
2023-05-08 18:29:58 +0300

    refactor(Chart): added drawPath()

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ChartOptions, ChartProps, Line } from './Chart.types';
 
 const defaultOptions: ChartOptions = {
@@ -25,78 +25,6 @@ export function Chart({ dpiRatio = 1, viewHeight, viewWidth, options = defaultOp
 
   const yMaxData = Math.max(...lines.map((line) => Math.max(...line.coords.map((coord) => coord.y))));
 
-  const initCanvas = (initialCanvas: HTMLCanvasElement) => {
-    initialCanvas.style.height = viewHeight + 'px';
-    initialCanvas.style.width = viewWidth + 'px';
-    initialCanvas.height = dpiViewHeight;
-    initialCanvas.width = dpiViewWidth;
-  };
-
-  const drawYSteps = (context: CanvasRenderingContext2D) => {
-    context.beginPath();
-    context.strokeStyle = '#dbdbdb';
-
-    const stepY = (dpiViewHeight - 2 * PADDING) / ROWS_COUNT;
-    const textStep = yMaxData / ROWS_COUNT;
-
-    for (let index = 1; index <= ROWS_COUNT; index++) {
-      const canvasY = dpiViewHeight - (stepY * index + PADDING);
-
-      const text = Math.ceil(textStep * index);
-
-      const textMarginX = 5;
-      const textMarginY = 25;
-      context.fillText(String(text), canvasXStart + textMarginX, canvasY + textMarginY);
-
-      context.moveTo(canvasXStart, canvasY);
-      context.lineTo(canvasXEnd, canvasY);
-    }
-
-    context.stroke();
-    context.closePath();
-  };
-
-  const initAxis = (context: CanvasRenderingContext2D) => {
-    context.beginPath();
-
-    context.moveTo(canvasXStart, canvasYEnd);
-    context.lineTo(canvasXStart, canvasYStart);
-    context.lineTo(canvasXEnd, canvasYStart);
-    context.stroke();
-
-    context.font = '24px mono';
-    context.fillText('0', dpiViewWidth - 16, dpiViewWidth + 16);
-
-    drawYSteps(context);
-
-    context.closePath();
-  };
-
-  const drawLine = (context: CanvasRenderingContext2D, lineData: Line) => {
-    const yRatio = (dpiViewHeight - 2 * PADDING) / yMaxData;
-
-    const { coords, color, width } = lineData;
-
-    context.beginPath();
-
-    context.lineWidth = width;
-    context.strokeStyle = color;
-
-    coords.forEach((coord) => {
-      const canvasY = dpiViewHeight - (coord.y * yRatio + PADDING);
-      const canvasX = coord.x + PADDING;
-
-      context.lineTo(canvasX, canvasY);
-    });
-
-    context.stroke();
-    context.closePath();
-  };
-
-  const drawChart = (context: CanvasRenderingContext2D) => {
-    lines.forEach((lineData) => drawLine(context, lineData));
-  };
-
   const getCanvasAndContext = (): {
     canvas: HTMLCanvasElement | null;
     context: CanvasRenderingContext2D | null | undefined;
@@ -104,6 +32,79 @@ export function Chart({ dpiRatio = 1, viewHeight, viewWidth, options = defaultOp
     const canvas = canvasRef.current;
     const context = canvas?.getContext('2d');
     return { canvas, context };
+  };
+
+  const initCanvas = (initialCanvas: HTMLCanvasElement) => {
+    initialCanvas.style.height = viewHeight + 'px';
+    initialCanvas.style.width = viewWidth + 'px';
+    initialCanvas.height = dpiViewHeight;
+    initialCanvas.width = dpiViewWidth;
+  };
+
+  const drawPath = (context: CanvasRenderingContext2D, callback: () => void) => {
+    context.beginPath();
+    callback();
+    context.closePath();
+  };
+
+  const drawYSteps = (context: CanvasRenderingContext2D) =>
+    drawPath(context, () => {
+      context.strokeStyle = '#dbdbdb';
+
+      const stepY = (dpiViewHeight - 2 * PADDING) / ROWS_COUNT;
+      const textStep = yMaxData / ROWS_COUNT;
+
+      for (let index = 1; index <= ROWS_COUNT; index++) {
+        const canvasY = dpiViewHeight - (stepY * index + PADDING);
+
+        const text = Math.ceil(textStep * index);
+
+        const textMarginX = 5;
+        const textMarginY = 25;
+        context.fillText(String(text), canvasXStart + textMarginX, canvasY + textMarginY);
+
+        context.moveTo(canvasXStart, canvasY);
+        context.lineTo(canvasXEnd, canvasY);
+      }
+
+      context.stroke();
+    });
+
+  const initAxis = (context: CanvasRenderingContext2D) =>
+    drawPath(context, () => {
+      context.moveTo(canvasXStart, canvasYEnd);
+      context.lineTo(canvasXStart, canvasYStart);
+      context.lineTo(canvasXEnd, canvasYStart);
+      context.stroke();
+
+      context.font = '24px mono';
+      context.fillText('0', dpiViewWidth - 16, dpiViewWidth + 16);
+
+      drawYSteps(context);
+    });
+
+  const drawLine = (context: CanvasRenderingContext2D, lineData: Line) => {
+    const yRatio = (dpiViewHeight - 2 * PADDING) / yMaxData;
+
+    const { coords, color, width } = lineData;
+
+    drawPath(context, () => {
+      context.lineWidth = width;
+      context.strokeStyle = color;
+
+      coords.forEach((coord) => {
+        const canvasY = dpiViewHeight - (coord.y * yRatio + PADDING);
+        const canvasX = coord.x + PADDING;
+
+        context.lineTo(canvasX, canvasY);
+      });
+
+      context.stroke();
+    });
+  };
+
+  const drawChart = (context: CanvasRenderingContext2D) => {
+    lines.forEach((lineData) => drawLine(context, lineData));
   };
 
   useEffect(() => {
