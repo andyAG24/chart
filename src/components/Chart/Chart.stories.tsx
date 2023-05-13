233a30a9ecf5b846d9ffb5dde6a1a4375863d324
andyAG24 <andy.ag.24@gmail.com>
2023-05-13 23:38:45 +0300

    feat(Chart): added story

---
+++ src/components/Chart/Chart.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Chart } from './Chart';
+import React from 'react';
+import { LinesMock } from './Chart.mock';
+
+const meta: Meta<typeof Chart> = {
+  title: 'Chart',
+  component: Chart,
+};
+
+export default meta;
+type Story = StoryObj<typeof Chart>;
+
+export const Main: Story = {
+  render: () => (
+    <Chart viewHeight={250} viewWidth={500} dpiRatio={2} options={{ padding: 50, rowsCount: 5 }} lines={LinesMock} />
+  ),
+};
